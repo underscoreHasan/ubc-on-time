cf4d6c98bfccb820b2cbc2cc33bebddf8640f0cb
underscoreHasan <hasan.nr49@gmail.com>
2023-06-08 15:10:10 +0500

    Completes barebones landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/UBCOnTime Logo.png';
+
+console.log(logo)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderImage />
+      <UploadButton />
+      <Information />
     </div>
   );
+}
+
+function HeaderImage() {
+  return (
+    <img class="logo" src={logo} alt="UBCOnTime Logo" width="1387" height="220" />
+  )
+}
+
+function UploadButton() {
+  return (
+    <button>cloud icon</button>
+  )
+}
+
+function Information() {
+  return (
+    <div>
+      <h2>Find your Timetable on the SSC, then click 'Download your schedule' and upload it here.</h2>
+      <p>Our app makes sure that don't have to sprint between your UBC classes.</p>
+    </div>
+  )
 }
 
 export default App;
