0043be45e7b20b1df5139c0ff8fa4b414188bedd
underscoreHasan <hasan.nr49@gmail.com>
2023-07-31 02:09:59 -0700

    Add static website disclaimer and staticResponse const

--- src/App.js
+++ src/App.js
@@ -27,18 +27,7 @@ function UploadButton({ onResponse, journeys }) {
   const [foundResponse, setFoundResponse] = useState(false);
 
   const handleClick = e => {
-    hiddenFileInput.current.click();
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    let file = e.target.files[0];
-    const content = await file.text();
-    const response = await fetch("http://localhost:9000/process", { method: "POST", body: content });
-
-    onResponse(JSON.parse(await response.text()).data);
-
+    onResponse(staticResponse);
     setFoundResponse(true);
   };
 
@@ -49,15 +38,9 @@ function UploadButton({ onResponse, journeys }) {
           <Button onClick={handleClick} variant="primary" className='uploadButton'>
             <img className="cloudIcon" src={cloudIcon} alt='cloud upload icon' />
           </Button>
-
-          <input
-            type="file"
-            style={{ display: 'none' }}
-            onChange={handleSubmit}
-            ref={hiddenFileInput}
-          />
         </>
-        : <Schedule journeys={journeys} />
+        :
+        <Schedule journeys={journeys} />
       }
     </>
   );
@@ -145,8 +128,143 @@ function Information() {
     <div className='information'>
       <h2>Find your Timetable on the SSC, then click 'Download your schedule' and upload it here.</h2>
       <p>Our app makes sure that don't have to sprint between your UBC classes.</p>
+      <p className='disclaimer'>For now, this version of the webapp is 'dumb' (makes no API calls/server requests) and is a demo for Github Pages.</p>
+      <p className='disclaimer'>Clicking the upload button will simply serve static data from a preselected .ics file.</p>
     </div>
   );
 }
 
 export default App;
+
+const staticResponse = [
+  {
+    "class1": "MATH 221 104",
+    "buil1": "Earth Sciences Building",
+    "class2": "CPSC 221 102",
+    "buil2": "West Mall Swing Space",
+    "day": "Mon",
+    "term": 1,
+    "time": "3 mins",
+    "online": false
+  },
+  {
+    "class1": "CPSC 213 L1B",
+    "buil1": "Institute for Computing (ICICS/CS)",
+    "class2": "CPSC 213 102",
+    "buil2": "Hugh Dempster Pavilion",
+    "day": "Tue",
+    "term": 1,
+    "time": "1 min",
+    "online": false
+  },
+  {
+    "class1": "MATH 221 104",
+    "buil1": "Earth Sciences Building",
+    "class2": "CPSC 221 102",
+    "buil2": "West Mall Swing Space",
+    "day": "Wed",
+    "term": 1,
+    "time": "3 mins",
+    "online": false
+  },
+  {
+    "class1": "CPSC 213 L1B",
+    "buil1": "Institute for Computing (ICICS/CS)",
+    "class2": "MATH 221 104",
+    "buil2": "Earth Sciences Building",
+    "day": "Fri",
+    "term": 1,
+    "time": "5 mins",
+    "online": false
+  },
+  {
+    "class1": "MATH 221 104",
+    "buil1": "Earth Sciences Building",
+    "class2": "CPSC 221 102",
+    "buil2": "West Mall Swing Space",
+    "day": "Fri",
+    "term": 1,
+    "time": "3 mins",
+    "online": false
+  },
+  {
+    "class1": "STAT 251 201",
+    "buil1": "Centre for Interactive  Research on Sustainability",
+    "class2": "STAT 251 L2C",
+    "buil2": "Earth Sciences Building",
+    "day": "Mon",
+    "term": 2,
+    "time": "4 mins",
+    "online": false
+  },
+  {
+    "class1": "STAT 251 L2C",
+    "buil1": "Earth Sciences Building",
+    "class2": "ENGL 110 011",
+    "buil2": "Buchanan",
+    "day": "Mon",
+    "term": 2,
+    "time": "8 mins",
+    "online": false
+  },
+  {
+    "class1": "ENGL 110 011",
+    "buil1": "Buchanan",
+    "class2": "MATH 200 204",
+    "buil2": "Chemistry",
+    "day": "Mon",
+    "term": 2,
+    "time": "4 mins",
+    "online": false
+  },
+  {
+    "class1": "STAT 251 201",
+    "buil1": "Centre for Interactive  Research on Sustainability",
+    "class2": "ENGL 110 011",
+    "buil2": "Buchanan",
+    "day": "Wed",
+    "term": 2,
+    "time": "11 mins",
+    "online": false
+  },
+  {
+    "class1": "ENGL 110 011",
+    "buil1": "Buchanan",
+    "class2": "MATH 200 204",
+    "buil2": "Chemistry",
+    "day": "Wed",
+    "term": 2,
+    "time": "4 mins",
+    "online": false
+  },
+  {
+    "class1": "CPSC 310 2W2",
+    "buil1": "",
+    "class2": "WRDS 150B W95",
+    "buil2": "",
+    "day": "Thu",
+    "term": 2,
+    "time": 0,
+    "online": true
+  },
+  {
+    "class1": "STAT 251 201",
+    "buil1": "Centre for Interactive  Research on Sustainability",
+    "class2": "ENGL 110 LT3",
+    "buil2": "Buchanan",
+    "day": "Fri",
+    "term": 2,
+    "time": "11 mins",
+    "online": false
+  },
+  {
+    "class1": "ENGL 110 LT3",
+    "buil1": "Buchanan",
+    "class2": "MATH 200 204",
+    "buil2": "Chemistry",
+    "day": "Fri",
+    "term": 2,
+    "time": "4 mins",
+    "online": false
+  }
+]
